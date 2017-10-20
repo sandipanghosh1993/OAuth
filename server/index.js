a0dd2e16e1109ef5093d36e862b0c03e6a62af2e
sandipanghosh1993 <sandipan.ghosh1993s@gmail.com>
2017-10-21 03:30:03 +0530

    oauth setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
 
 const app = express();
 
-// clientID 658821072663-dsbsnefpckak7bv9n6868r3c9tbgovpi.apps.googleusercontent.com
-// clientSecret v9dErH_z7dpzUqb-wtZOOs4r
-passport.use(new GoogleStrategy());
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
